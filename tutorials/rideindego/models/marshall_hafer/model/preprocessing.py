3d08712c4ef7fbeb622b64b5df7e36cd743423dd
LM <hi@leima.is>
2021-05-09 17:13:41 +0200

    updated rf

--- tutorials/rideindego/models/marshall_hafer/model/preprocessing.py
+++ tutorials/rideindego/models/marshall_hafer/model/preprocessing.py
@@ -5,78 +5,14 @@ import click
 import pandas as pd
 import simplejson as json
 from dotenv import load_dotenv
+from haferml.blend.config import Config
+from haferml.preprocess.ingredients import attributes
+from haferml.preprocess.pipeline import BasePreProcessor
+from haferml.sync.local import prepare_folders
 from loguru import logger
 
 logger.info(f"Experiment started at: {datetime.datetime.now()}")
 load_dotenv()
-
-
-def load_config(config_path):
-    """
-    load_config loads the config files of the project
-
-    :param config_path: path to the config file
-    :type config_path: str, optional
-    """
-
-    if not os.path.exists(config_path):
-        raise Exception(
-            f"config file path {config_path} does not exist! Beware of the relative path."
-        )
-
-    logger.debug(f"Loading config from {config_path}")
-    with open(config_path, "r") as fp:
-        config = json.load(fp)
-
-    if not config:
-        logger.warning(f"The config is empty: {config}")
-
-    return config
-
-
-def get_config(configs, path):
-    """
-    Get value of the configs under specified path
-
-    :param dict configs: input dictionary
-    :param list path: path to the value to be obtained
-
-    >>> get_config({'etl':{'raw':{'local':'data/raw', 'remote': 's3://haferml-tutorials/rideindego/marshall/data/raw'}}},['etl','raw'])
-    {'local':'data/raw', 'remote': 's3://haferml-tutorials/rideindego/marshall/data/raw'}
-    """
-
-    # Construct the path
-    if not isinstance(path, (list, tuple)):
-        logger.warning(f"path is not list nor tuple, converting to list: {path}")
-        path = [path]
-
-    # Find the values
-    res = configs.copy()
-    for p in path:
-        res = res[p]
-
-    return res
-
-
-def construct_paths(config, base_folder=os.getenv("BASE_FOLDER")):
-    """
-    construct_paths reconstructs the path based on base folder
-    """
-
-    if not config.get("local"):
-        logger.warning(f"{config} does not contain local key ")
-        return config
-
-    config_recon = {}
-    config_local = config["local"]
-    config_name = config.get("name")
-    config_local = os.path.join(base_folder, config_local)
-    config_recon["local"] = config_local
-    if config_name:
-        config_local_full = os.path.join(config_local, config_name)
-        config_recon["file_path"] = config_local_full
-
-    return {**config, **config_recon}
 
 
 def load_data(data_path):
@@ -88,7 +24,7 @@ def load_data(data_path):
     return dataframe
 
 
-class Preprocess:
+class Preprocess(BasePreProcessor):
     """
     Preprocess dataset
 
@@ -96,28 +32,24 @@ class Preprocess:
     """
 
     def __init__(self, config):
-        self.config = config
+        super(Preprocess, self).__init__(config=config)
         self.feature_cols = self.config["features"]
         self.target_cols = self.config["targets"]
 
+    @attributes(order=1)
     def _drop_unused_columns(self, dataframe):
 
-        dataframe = dataframe[self.feature_cols + self.target_cols]
+        self.dataframe = dataframe[self.feature_cols + self.target_cols]
 
-        return dataframe
+        return self.dataframe
 
-    def run(self, dataframe):
-
-        dataframe = self._drop_unused_columns(dataframe)
-
-        return dataframe
 
 
 @click.command()
 @click.option(
     "-c",
     "--config",
-    type=click.Path(exists=True),
+    type=str,
     default=os.getenv("CONFIG_FILE"),
     help="Path to config file",
 )
@@ -125,35 +57,25 @@ def preprocess(config):
 
     base_folder = os.getenv("BASE_FOLDER")
 
-    _CONFIG = load_config(config)
+    _CONFIG = Config(config, base_folder=base_folder)
 
-    preprocessed_data_config = get_config(
-        _CONFIG, ["preprocessing", "dataset", "preprocessed"]
-    )
-    preprocessed_data_config = construct_paths(preprocessed_data_config, base_folder)
+    preprocessed_data_config = _CONFIG[["preprocessing", "dataset", "preprocessed"]]
+    transformed_trip_data_config = _CONFIG[["etl", "transformed", "trip_data"]]
 
-    transformed_trip_data_config = get_config(
-        _CONFIG, ["etl", "transformed", "trip_data"]
-    )
-    transformed_trip_data_config = construct_paths(
-        transformed_trip_data_config, base_folder
-    )
     # create folders
-    if not os.path.exists(preprocessed_data_config["local"]):
-        os.makedirs(preprocessed_data_config["local"])
-    if not os.path.exists(transformed_trip_data_config["local"]):
-        os.makedirs(transformed_trip_data_config["local"])
+    prepare_folders(preprocessed_data_config["local"], base_folder=base_folder)
+    prepare_folders(transformed_trip_data_config["local"], base_folder=base_folder)
 
     # load transformed data
-    df = load_data(transformed_trip_data_config["file_path"])
+    df = load_data(transformed_trip_data_config["name_absolute"])
 
     # preprocess
-    pr = Preprocess(config=get_config(_CONFIG, ["preprocessing"]))
+    pr = Preprocess(config=_CONFIG[["preprocessing"]])
     df = pr.run(df)
 
     # save
-    df.to_parquet(preprocessed_data_config["file_path"], index=False)
-    logger.info(f'Saved preprocessed data to {preprocessed_data_config["file_path"]}')
+    df.to_parquet(preprocessed_data_config["name_absolute"], index=False)
+    logger.info(f'Saved preprocessed data to {preprocessed_data_config["name_absolute"]}')
 
     return df
 
--- tutorials/rideindego/models/marshall_hafer/model/rf.py
+++ tutorials/rideindego/models/marshall_hafer/model/rf.py
@@ -1,89 +1,24 @@
 import datetime
-from logging import log
 import os
 
+import category_encoders as ce
 import click
 import joblib
+import numpy as np
 import pandas as pd
 import simplejson as json
 from dotenv import load_dotenv
+from haferml.blend.config import Config
+from haferml.model.pipeline import DataSetX, ModelSetX, ModelWorkflowX
+from haferml.sync.local import prepare_folders
 from loguru import logger
+from sklearn import metrics
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import RandomizedSearchCV, train_test_split
 from sklearn.pipeline import Pipeline
-from sklearn import metrics
-import numpy as np
-import category_encoders as ce
 
 logger.info(f"Experiment started at: {datetime.datetime.now()}")
 load_dotenv()
-
-
-def load_config(config_path):
-    """
-    load_config loads the config files of the project
-
-    :param config_path: path to the config file
-    :type config_path: str, optional
-    """
-
-    if not os.path.exists(config_path):
-        raise Exception(
-            f"config file path {config_path} does not exist! Beware of the relative path."
-        )
-
-    logger.debug(f"Loading config from {config_path}")
-    with open(config_path, "r") as fp:
-        config = json.load(fp)
-
-    if not config:
-        logger.warning(f"The config is empty: {config}")
-
-    return config
-
-
-def get_config(configs, path):
-    """
-    Get value of the configs under specified path
-
-    :param dict configs: input dictionary
-    :param list path: path to the value to be obtained
-
-    >>> get_config({'etl':{'raw':{'local':'data/raw', 'remote': 's3://haferml-tutorials/rideindego/marshall/data/raw'}}},['etl','raw'])
-    {'local':'data/raw', 'remote': 's3://haferml-tutorials/rideindego/marshall/data/raw'}
-    """
-
-    # Construct the path
-    if not isinstance(path, (list, tuple)):
-        logger.warning(f"path is not list nor tuple, converting to list: {path}")
-        path = [path]
-
-    # Find the values
-    res = configs.copy()
-    for p in path:
-        res = res[p]
-
-    return res
-
-
-def construct_paths(config, base_folder=os.getenv("BASE_FOLDER")):
-    """
-    construct_paths reconstructs the path based on base folder
-    """
-
-    if not config.get("local"):
-        raise Exception(f"{config} does not contain local key ")
-
-    config_recon = {}
-    config_local = config["local"]
-    config_name = config.get("name")
-    config_local = os.path.join(base_folder, config_local)
-    config_recon["local"] = config_local
-    if config_name:
-        config_local_full = os.path.join(config_local, config_name)
-        config_recon["file_path"] = config_local_full
-
-    return {**config, **config_recon}
 
 
 def load_data(data_path):
@@ -117,27 +52,24 @@ def isoencode(obj):
         return bool(obj)
 
 
-class DataSet:
+class DataSet(DataSetX):
     """
     DataSet for the model
     """
 
     def __init__(self, config, base_folder):
-        self.config = config
-        self.base_folder = base_folder
+        super(DataSet, self).__init__(config, base_folder)
 
-        self.pred_cols = self.config.get("targets")
-        self.feature_cols = self.config.get("features")
+        self.targets = self.config.get("targets")
+        self.features = self.config.get("features")
         self.cat_cols = self.config.get("encoding", {}).get("categorical_columns")
 
         self.test_size = self.config.get("test_size")
         self.random_state = self.config.get("random_state")
 
-        self.artifacts = self.config["artifacts"]
-
         logger.debug(
-            f"features: {self.feature_cols}\n"
-            f"predict: {self.pred_cols}\n"
+            f"features: {self.features}\n"
+            f"predict: {self.targets}\n"
             f"base folder: {self.base_folder}\n"
             f"artifacts configs: {self.artifacts}"
         )
@@ -151,8 +83,8 @@ class DataSet:
         self.data = data
         logger.debug(f"length of dataset: {len(self.data)}\n")
 
-        self.X = self.data.loc[:, self.feature_cols]
-        self.y = self.data.loc[:, self.pred_cols]
+        self.X = self.data.loc[:, self.features]
+        self.y = self.data.loc[:, self.targets]
 
         # No information leak here as we use Binary Encoder only
         self._encode()
@@ -174,57 +106,21 @@ class DataSet:
         # save the train test data
         self._export_train_test_data()
 
-    def _export_train_test_data(self):
-        """
-        _export_train_test_data saves train and test datasets
-        """
 
-        dataset_folder = self.artifacts["dataset"]["local"]
-
-        ## Save the train and test datasets
-        logger.info("Export test and train data")
-        self.X_train.to_parquet(
-            os.path.join(self.base_folder, dataset_folder, "model_X_train.parquet")
-        )
-
-        self.X_test.to_parquet(
-            os.path.join(self.base_folder, dataset_folder, "model_X_test.parquet")
-        )
-
-        pd.DataFrame(self.y_train, columns=self.pred_cols).to_parquet(
-            os.path.join(self.base_folder, dataset_folder, "model_y_train.parquet")
-        )
-
-        pd.DataFrame(self.y_test, columns=self.pred_cols).to_parquet(
-            os.path.join(self.base_folder, dataset_folder, "model_y_test.parquet")
-        )
-
-        # Save dataset locally
-        self.data.to_parquet(
-            os.path.join(self.base_folder, dataset_folder, "dataset.parquet")
-        )
-
-
-class ModelSet:
+class ModelSet(ModelSetX):
     """
     The core of the model including hyperparameters
     """
 
     def __init__(self, config, base_folder):
-        self.config = config
-        self.base_folder = base_folder
-
-        self.pred_cols = self.config.get("targets")
-        self.feature_cols = self.config.get("features")
+        super(ModelSet, self).__init__(config, base_folder)
 
         self.test_size = self.config.get("test_size")
         self.random_state = self.config.get("random_state")
 
-        self.artifacts = self.config["artifacts"]
-
         logger.debug(
-            f"features: {self.feature_cols}\n"
-            f"predict: {self.pred_cols}\n"
+            f"features: {self.features}\n"
+            f"predict: {self.targets}\n"
             f"base folder: {self.base_folder}\n"
             f"artifacts configs: {self.artifacts}"
         )
@@ -232,7 +128,6 @@ class ModelSet:
     def create_model(self):
 
         logger.info("Setting up hyperparameters ...")
-        self._combine_hyperparameters()
 
         logger.info("Create pipeline")
         rf = RandomForestRegressor(
@@ -247,11 +142,11 @@ class ModelSet:
             self.pipeline,
             cv=self.config.get("cv", {}).get("folds", 3),
             n_iter=self.config.get("cv", {}).get("n_iter", 5),
-            param_distributions=self.hyperparams_grid,
+            param_distributions=self.hyperparameters,
             verbose=6,
         )
 
-    def _combine_hyperparameters(self):
+    def _set_hyperparameters(self):
         hyperparams_grid = self.config.get("hyperparameters")
         if hyperparams_grid is None:
             hyperparams_grid = self._create_hyperparameter_space()
@@ -261,7 +156,8 @@ class ModelSet:
                 **hyperparams_grid,
             }
             logger.info(f"Using hyperparameters: \n{hyperparams_grid}")
-        self.hyperparams_grid = hyperparams_grid
+
+        return hyperparams_grid
 
     @staticmethod
     def _create_hyperparameter_space():
@@ -301,91 +197,12 @@ class ModelSet:
         return rf_random_grid
 
 
-class RandomForest:
+class RandomForest(ModelWorkflowX):
     """A model to predict the duration"""
 
     def __init__(self, config, dataset, modelset, base_folder=os.getenv("BASE_FOLDER")):
+        super(RandomForest, self).__init__(config, dataset, modelset, base_folder)
         self.name = "marshall random forest"
-        self.config = config
-        self.base_folder = base_folder
-        self.artifacts = self.config["artifacts"]
-        self.DataSet = dataset
-        self.ModelSet = modelset
-
-    def fit_and_report(self):
-        """
-        _fit_and_report fits the model using input data and generate reports
-        """
-
-        logger.info("Fitting the model")
-        logger.debug(
-            "Shape of train data:\n"
-            f"X_train: {self.DataSet.X_train.shape}, {self.DataSet.X_train.sample(3)}\n"
-            f"y_train: {self.DataSet.y_train.shape}, {self.DataSet.y_train.sample(3)}"
-        )
-        self.ModelSet.model.fit(
-            self.DataSet.X_train.squeeze(), self.DataSet.y_train.squeeze()
-        )
-
-        logger.info("Reporting results")
-        train_score = self.ModelSet.model.score(
-            self.DataSet.X_train, self.DataSet.y_train
-        )
-        test_score = self.ModelSet.model.score(self.DataSet.X_test, self.DataSet.y_test)
-
-        self.report = {
-            "hyperparameters": self.ModelSet.hyperparams_grid,
-            "best_params": self.ModelSet.model.best_params_,
-            "cv_results": self.ModelSet.model.cv_results_,
-            "test_score": test_score,
-            "train_score": train_score,
-        }
-
-        logger.debug(self.report)
-
-    def export_results(self):
-        """
-        export_results saves the necessary artifacts
-        """
-        model_folder = self.artifacts["model"]["local"]
-        if not os.path.exists(os.path.join(self.base_folder, model_folder)):
-            os.makedirs(os.path.join(self.base_folder, model_folder))
-
-        logger.info("Preserve models")
-        joblib.dump(
-            self.ModelSet.model,
-            os.path.join(
-                self.base_folder,
-                model_folder,
-                self.artifacts["model"]["name"],
-            ),
-        )
-
-        logger.info("Perserve logs")
-        log_file_path = os.path.join(
-            self.base_folder,
-            model_folder,
-            f'{self.artifacts["model"]["name"]}.log',
-        )
-        logger.info(f"Save log file to {log_file_path}")
-        with open(log_file_path, "a+") as fp:
-            json.dump(self.report, fp, default=isoencode)
-            fp.write("\n")
-        logger.info(f"Saved logs")
-
-    def train(self, dataset):
-        """
-        train connects the training workflow
-        """
-
-        logger.info("1. Create train test datasets")
-        self.DataSet.create_train_test_datasets(dataset)
-        logger.info("2. Create model")
-        self.ModelSet.create_model()
-        logger.info("3. Fit model and report")
-        self.fit_and_report()
-        logger.info("4. Export results")
-        self.export_results()
 
 
 class Reload:
@@ -431,7 +248,7 @@ class Reload:
 @click.option(
     "-c",
     "--config",
-    type=click.Path(exists=True),
+    type=str,
     default=os.getenv("CONFIG_FILE"),
     help="Path to config file",
 )
@@ -439,20 +256,17 @@ def preprocess(config):
 
     base_folder = os.getenv("BASE_FOLDER")
 
-    _CONFIG = load_config(config)
-    preprocessed_data_config = get_config(
-        _CONFIG, ["preprocessing", "dataset", "preprocessed"]
-    )
-    preprocessed_data_config = construct_paths(preprocessed_data_config, base_folder)
-    rf_config = get_config(_CONFIG, ["model", "rf"])
+    _CONFIG = Config(config, base_folder=base_folder)
+    preprocessed_data_config = _CONFIG[["preprocessing", "dataset", "preprocessed"]]
+    rf_config = _CONFIG[["model", "rf"]]
 
     # create folders
-    if not os.path.exists(preprocessed_data_config["local"]):
-        os.makedirs(preprocessed_data_config["local"])
+    prepare_folders(preprocessed_data_config["local"], base_folder=base_folder)
+    prepare_folders(_CONFIG[["model", "rf", "artifacts", "model", "local"]], base_folder=base_folder)
 
     # load transformed data
     logger.debug(f"Loading data ...")
-    df = load_data(preprocessed_data_config["file_path"]).sample(1000)
+    df = load_data(preprocessed_data_config["name_absolute"]).sample(1000)
 
     # model
     logger.debug(f"Prepare modelset and dataset")
